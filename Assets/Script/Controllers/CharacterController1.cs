4f20877aed9b1c1f49d10ce69b7d5f43119c5b63
qrran <dan.lin001@stu.bmcc.cuny.edu>
2024-05-24 18:58:49 -0400

    character controller class

--- Assets/Script/Controllers/CharacterController1.cs
+++ Assets/Script/Controllers/CharacterController1.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 using Unity.Burst.CompilerServices;
 //using System.Numerics;
 using UnityEngine;
+using UnityEngine.UIElements;
+
 
 public class CharacterController1 : MonoBehaviour
 {
-	//1.position
-
+	//used to manage the state of the character
 	private CharacterControllerState currentState;
 
-	private const float rotateDegreesPerSecond = 2f;
-	private const float moveSpeed = 2f;
-	float rotationAmount;
-	Vector3 newPosition;
+	//swipe cane
+	private const float rotateDegreesPerSecond = 5f;
+	private Quaternion targetRotation;
+	private bool rotateClockwise = true;
 
-	bool isMoving;
+	//move 
+	private const float moveSpeed = 1f;
+	private float rotationAmount;
+	private Vector3 newPosition;
 
+	//raycast
 	RaycastHit2D hit;
+	//distance for raycast
+	private float distance = 1f;
+
+	//final position in the scene
+	Vector3 targetPosition;
+
+	//controls the cane
+	bool isSwippingCane;
+	//manage movement after detected obstacle
+	//bool obstacleDetected;
+
+	Vector3 direction;
 
 	public enum CharacterControllerState
 	{
@@ -26,19 +43,20 @@ public class CharacterController1 : MonoBehaviour
 
 	void Start()
 	{
+		isSwippingCane = true;
+		//obstacleDetected = false;
 		rotationAmount = rotateDegreesPerSecond * Time.deltaTime;
-
 		currentState = CharacterControllerState.forward;
+
 	}
 
 	private void Update()
 	{
-		// Check if the current state is stop
-		if (currentState == CharacterControllerState.stop)
-		{
-			// If the character is stopped, return without performing any movement actions
-			return;
-		}
+
+		TargetPosition();
+
+		if (isSwippingCane)
+			SwipCane();
 
 		switch (currentState)
 		{
@@ -59,35 +77,52 @@ public class CharacterController1 : MonoBehaviour
 				break;
 		}
 
-		if (CannotMoveForward())
-		{
-			currentState = CharacterControllerState.down;
-		}
-		//else
-		//{
-		//	currentState = CharacterControllerState.forward;
-		//}
-		//currentState = CharacterControllerState.forward;
-
 	}
 
-	//public void SwipCane()
+	//private void FixedUpdate()
 	//{
-	//	float rotationAmount = rotateDegreesPerSecond * Time.deltaTime;
-	//switch (currentState)
-	//{
-	//	case CharacterControllerState.Idle:
-	//		Idle();
-	//		break;
-	//	case CharacterControllerState.Walk:
-	//		Walk();
-	//		break;
-	//	case CharacterControllerState.DetectedObstacle:
-	//		DetectedObstacle();
-	//		break;
-	//}
+	//	// Update the ray direction based on the rotated angle
+	//	Vector2 rayDirection = Quaternion.Euler(0f, 0f, rotateClockwise ? 45f : 315f) * Vector2.right;
+
+	//	// Cast a ray in the specified direction
+	//	RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDirection, distance);
+
+	//	// Check if the ray hits an obstacle
+	//	if (hit.collider != null)
+	//	{
+	//		Debug.Log("Obstacle detected at angle: " + (rotateClockwise ? 45f : 315f));
+	//		obstacleDetected = true;
+	//	}
+	//	else
+	//	{
+	//		obstacleDetected = false;
+	//		Debug.Log("obstacles not detected");
+	//		Debug.DrawRay(transform.position, rayDirection * 2f, Color.blue);
+	//	}
+
+	//	// Check if the rotation has reached the target
+	//	if (Quaternion.Angle(transform.rotation, targetRotation) < 0.1f)
+	//	{
+	//		// Change rotation direction
+	//		rotateClockwise = !rotateClockwise;
+	//	}
 	//}
 
+	public void SwipCane()
+	{
+		// Calculate the target rotation based on current rotation
+		targetRotation = Quaternion.Euler(0f, 0f, rotateClockwise ? 45f : 315f);
+
+		// Rotate towards the target rotation
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationAmount);
+
+		// Check if the rotation has reached the target
+		if (Quaternion.Angle(transform.rotation, targetRotation) < 0.1f)
+		{
+			// Change rotation direction
+			rotateClockwise = !rotateClockwise;
+		}
+	}
 	public void Forward()
 	{
 		//Debug.Log("moving forward");
@@ -100,12 +135,12 @@ public class CharacterController1 : MonoBehaviour
 		Debug.Log("Down");
 		newPosition = new Vector3(0, -moveSpeed * Time.deltaTime, 0);
 		transform.position += newPosition;
-
 	}
 	public void Right()
 	{
 		Debug.Log("Right");
 		transform.Rotate(Vector3.back, rotationAmount);
+		currentState = CharacterControllerState.forward;
 	}
 	public void Left()
 	{
@@ -118,55 +153,60 @@ public class CharacterController1 : MonoBehaviour
 		Debug.Log("Stop moving");
 		newPosition = new Vector3(0, 0, 0);
 		transform.position += newPosition;
-		// Set the current state to stop
-		//currentState = CharacterControllerState.stop;
-		//transform.position = collisionPoint;
 	}
-
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-
-		currentState = CharacterControllerState.down;
-
-		//collision.gameObject.CompareTag("Obstacle");
-
+		isSwippingCane = false;
+		currentState = CharacterControllerState.stop;
 	}
-
-	//private void OnCollisionStay2D(Collision2D collision)
-	//{
-	//	//currentState = CharacterControllerState.down;
-	//}
-
+	private void OnCollisionStay2D(Collision2D collision)
+	{
+		currentState = CharacterControllerState.down;
+	}
 	private void OnCollisionExit2D(Collision2D collision)
 	{
-		//currentState = CharacterControllerState.forward;
-		//Debug.Log("oncolision2d moving down");
+		isSwippingCane = true;
 		currentState = CharacterControllerState.forward;
+	}
 
+	void TargetPosition()
+	{
+		GameObject target = GameObject.Find("TargetPosition");
+		//Debug.Log(target.transform.position);
+		direction = new Vector2(target.transform.position.x - transform.position.x,
+									target.transform.position.y - transform.position.y);
+
+		float distanceToTarget = Vector2.Distance(transform.position, targetPosition);
+
+		float stoppingDistance = 1f;
+
+		if (distanceToTarget <= stoppingDistance)
+		{
+			// Stop the character
+			currentState = CharacterControllerState.stop;
+		}
 	}
 
 	bool CannotMoveForward()
 	{
-		//RaycastHit2D hit;
-		//if (Physics.Raycast(transform.position, transform.forward, out hit, 2f))
-		//{
-		//	Debug.Log("cannot move forward, obstacles detected");
-		//	return true;
-		//}
-		//return false;
-		//if (Physics2D.Raycast(transform.position, Vector2.right))
-		//{
-		//	Debug.Log("cannot move forward, obstacles detected");
-		//	return true;
-		//}
-		//return false;
-		hit = Physics2D.Raycast(transform.position, Vector2.right, 0.1f); // Cast a ray to the right with a distance of 1 unit
+		hit = Physics2D.Raycast(transform.position, Vector2.right, distance); // Cast a ray to the right with a distance of 1 unit
 
-		if (hit.collider != null && hit.collider.CompareTag("Obstacle"))
+		//(Physics.Raycast(transform.position, transform.forward, out hit, 2f
+		if (hit.collider != null)
 		{
 			Debug.Log("Cannot move forward, obstacles detected");
+			// Draw a debug line from the ray's origin to the hit point
+			Debug.DrawRay(transform.position, hit.point, Color.red);
+
+			// Optionally, you can also draw a debug sphere at the hit point to visualize it
+			Debug.DrawRay(hit.point, hit.normal, Color.blue, 0.1f);
 			return true;
 		}
-		return false;
+		else
+		{
+			Debug.DrawRay(transform.position, transform.position + transform.right * distance, Color.blue);
+			Debug.Log("didn't hit");
+			return false;
+		}
 	}
 }
